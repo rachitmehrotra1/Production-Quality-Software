38bd399674c5dd3e449eb59fa6206aef41816c1c
Rachit Mehrotra <rm4149@nyu.edu>
2017-02-04 17:02:35 -0500

    Adding the JSON Simple jar for writing and reading address book

--- PS1/src/edu/nyu/cs/pqs/ps1/AddressBook.java
+++ PS1/src/edu/nyu/cs/pqs/ps1/AddressBook.java
@@ -1,6 +1,8 @@
 package edu.nyu.cs.pqs.ps1;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class AddressBook {
   // AddressBook variable to store all the Address Book Entries. Choosing HasSet due to the fast
@@ -29,6 +31,78 @@ public class AddressBook {
 
   public boolean removeEntry(AddressBookEntry singleEntry) {
     return addressBook.remove(singleEntry);
+  }
+
+  public List<AddressBookEntry> searchByName(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getName().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+  }
+
+  public List<AddressBookEntry> searchByPhoneNumber(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getPhoneNumber().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+  }
+
+  public List<AddressBookEntry> searchByPostalAddress(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getPostalAddress().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+  }
+
+  public List<AddressBookEntry> searchByEmailAddress(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getEmailAddress().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+  }
+
+  public List<AddressBookEntry> searchByNote(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getNote().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+  }
+
+  public List<AddressBookEntry> searchAddressBookEntry(String searchString) {
+    List<AddressBookEntry> result = new ArrayList<AddressBookEntry>();
+    for (AddressBookEntry entry : addressBook) {
+      if (entry.getName().equals(searchString) || entry.getEmailAddress().equals(searchString)
+          || entry.getNote().equals(searchString) || entry.getPhoneNumber().equals(searchString)
+          || entry.getPostalAddress().equals(searchString)) {
+        result.add(entry);
+      }
+    }
+    return result;
+
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder addressBookString = new StringBuilder();
+    for (AddressBookEntry entry : addressBook) {
+      addressBookString.append(entry.toString());
+    }
+    return addressBookString.toString();
   }
 
 
