4dad1693421c5cd047de4f0f1fae7c99698231eb
Rachit Mehrotra <rm4149@nyu.edu>
2017-02-25 22:24:41 -0500

    Finished the StopWatchImplementation and checked correctness. TODO: Java Docs and hashcode,string,equals function

--- PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchImpl.java
+++ PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchImpl.java
@@ -1,8 +1,11 @@
 package edu.nyu.pqs.stopwatch.impl;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import edu.nyu.pqs.stopwatch.api.Stopwatch;
+import edu.nyu.pqs.stopwatch.api.StopwatchState;
 
 /**
  * 
@@ -10,45 +13,78 @@ import edu.nyu.pqs.stopwatch.api.Stopwatch;
  *
  */
 public class StopwatchImpl implements Stopwatch {
+  
+  private final String id;
+  private StopwatchState stopwatchState;
+  private Object lock = new Object();
+  private List<Long> lapTimes;
+  private long startTime;
 
   public StopwatchImpl(String id) {
-    // TODO Auto-generated constructor stub
+    this.id=id;
+    stopwatchState = StopwatchState.RESET;
+    lapTimes = new LinkedList<Long>();
+    startTime=0;
   }
 
   @Override
   public String getId() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.id;
   }
 
   @Override
   public void start() {
-    // TODO Auto-generated method stub
-
+    synchronized (lock) {
+      if (stopwatchState == StopwatchState.RUNNING){
+        throw new IllegalStateException("Id:"+id+" is already running");
+      }
+      if (lapTimes.size()==0){
+        startTime =System.currentTimeMillis();
+      }
+      stopwatchState = StopwatchState.RUNNING;
+    }
   }
 
   @Override
   public void lap() {
-    // TODO Auto-generated method stub
-
+    synchronized(lock){
+      if(stopwatchState != StopwatchState.RUNNING){
+        throw new IllegalStateException("Id:"+id+" is not running!");
+      }
+      long currentTime = System.currentTimeMillis();
+      long lapTime = currentTime - startTime ;
+      lapTimes.add(lapTime);
+      startTime = currentTime;
+      
+    }
   }
 
   @Override
   public void stop() {
-    // TODO Auto-generated method stub
-
+    synchronized(lock){
+      if(stopwatchState != StopwatchState.RUNNING){
+        throw new IllegalStateException("Id:"+id+" is not running!");
+      }
+      lap();
+      stopwatchState = StopwatchState.STOP;
+      
+    }
   }
 
   @Override
   public void reset() {
-    // TODO Auto-generated method stub
-
+    synchronized(lock){
+      lapTimes.clear();
+      stopwatchState = StopwatchState.RESET;
+      
+    }
   }
 
   @Override
   public List<Long> getLapTimes() {
-    // TODO Auto-generated method stub
-    return null;
+    synchronized (lock){
+      return Collections.unmodifiableList(lapTimes);
+    }
   }
 
 }
